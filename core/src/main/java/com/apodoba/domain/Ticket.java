f4c5401510cf092624801eb70674b8ef21868c59
apodoba <Arina.Podoba@dataart.com>
2014-04-03 17:04:47 +0300

    Change derby url connection

--- core/src/main/java/com/apodoba/domain/Ticket.java
+++ core/src/main/java/com/apodoba/domain/Ticket.java
@@ -3,8 +3,8 @@ package com.apodoba.domain;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "TICKET")
+//@Entity
+//@Table(name = "TICKET")
 public class Ticket {
 
 }

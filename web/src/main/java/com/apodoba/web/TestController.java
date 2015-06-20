a5a963527647d3bd69ebc9a9b70f02a57937ffe2
arina <arinapodoba@gmail.com>
2015-06-21 00:49:21 +0300

    Added test controller

--- web/src/main/java/com/apodoba/web/TestController.java
+++ web/src/main/java/com/apodoba/web/TestController.java
@@ -1,13 +1,20 @@
-package com.apodoba;
+package com.apodoba.web;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.apodoba.service.TestService;
+
+@Controller
+@RequestMapping(value = "/test")
+public class TestController {
+
+	@Autowired
+	private TestService testService;
+
+	@RequestMapping(value = "/tickets")
+	public int getAllTickets() {
+		return testService.getAllTickets().size();
+	}
 }
---
+++ web/src/main/webapp/WEB-INF/views/views.jsp
@@ -0,0 +1,4 @@
+<h1>1. Test CSS</h1>
+ 
+<h2>2. Test JS</h2>
+<div id="msg"></div>
